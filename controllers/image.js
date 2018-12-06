30437634524d5f9f29c14ddf96b01831a4db24dc
pgb4 <preston.bebas1@gmail.com>
2018-12-06 16:15:36 -0500

    made api key config var

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'b60c3f919c674bfbad76549811c52d94'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
